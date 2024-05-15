e5cf631d66c5b449cb5598feb8354ddcde2eaf3b
Puthsitha <puthsithamouern@gmail.com>
2024-05-15 10:07:08 +0700

    feat : drop cart table

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -9,23 +9,7 @@ const createOrder = async (req: Request, res: Response) => {
   CreatOrderSchema.parse(req.body);
   // 1. create transaction
   await prismaClient.$transaction(async (tx) => {
-    // 2. list all cart
-    const cart = await tx.cart.findMany({
-      where: {
-        userId: req.user.id,
-      },
-      include: {
-        product: true,
-      },
-    });
-    if (cart.length == 0) {
-      throw new NotFoundException(false, "Cart is Empty", ErrorCode.NOT_FOUNT);
-    }
-    // 3. calculate total amount
-    const price = cart.reduce((prev, current) => {
-      return prev + current.qty * Number(current.product.price);
-    }, 0);
-    // 4. fetch user addresss
+    // fetch user addresss
     const address = await tx.address.findFirst({
       where: {
         id: req.body.addressId,
@@ -33,34 +17,17 @@ const createOrder = async (req: Request, res: Response) => {
     });
 
     if (address) {
-      //6. creaet order
       const order = await tx.order.create({
         data: {
           userId: req.user.id,
-          amount: price,
-          // 5. define formatedAddress in index.ts (extent prisma)
+          amount: 1,
           addressId: address.id,
-          // address: String(address?.formattedAddress),
-          products: {
-            create: cart.map((cart) => {
-              return {
-                productId: cart.productId,
-                qty: cart.qty,
-              };
-            }),
-          },
         },
       });
-      // 7. order even
+      // order even
       await tx.orderEvent.create({
         data: {
           orderId: order.id,
-        },
-      });
-      //8. empty cart
-      await tx.cart.deleteMany({
-        where: {
-          userId: req.user.id,
         },
       });
       return res.json({ message: true, data: "Order Successfully!" });
